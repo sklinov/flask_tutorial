c6a360790014cebc3fbefb9797ed5b38e33cfa6f
Sergei <me@sklinov.pro>
2019-09-07 17:58:11 +0500

    p1p2

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Text text text text text',
+            'author': 'Sergei Klinov',
+            'create_date': '07-09-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Text text text text text',
+            'author': 'Marina Kostareva',
+            'create_date': '05-09-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Text text text text text',
+            'author': 'Mnyams',
+            'create_date': '01-09-2019'
+        },
+    ]
+    return articles
